76d265ed6d536498abde873b02f7e6915592e86f
Pranav5738 <pranavshah5738@gmail.com>
2025-12-22 16:13:04 +0530

    Add files via upload

---
+++ MergeSortedArrays/MergeSortedArrays.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void swapIfGreater(vector<int>& a, vector<int>& b, int i, int j) {
+        if (a[i] > b[j]) {
+            swap(a[i], b[j]);
+        }
+    }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // left in nums1, right in nums2
+                if (left < m && right >= m) {
+                    swapIfGreater(nums1, nums2, left, right - m);
+                }
+                // both in nums2
+                else if (left >= m) {
+                    swapIfGreater(nums2, nums2, left - m, right - m);
+                }
+                // both in nums1
+                else {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+
+                left++;
+                right++;
+            }
+
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
+        }
+
+        // copy nums2 back to nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+    }
+};
